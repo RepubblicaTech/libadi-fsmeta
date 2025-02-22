11ca824eb58a2ddad91e83af8906da6be7e1ca56
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2025-02-22 09:45:10 +0100

    Initial fs metalang implementation (incomplete ofc)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Fork of libADI for filesystem metalang implementation
+
+# How to create your own metalang for ADI
---
+++ metalanguages/fs/disk.h
@@ -0,0 +1,26 @@
+#ifndef __DISK_H__
+#define __DISK_H__
+
+#include "../../types.h"
+#include "../misc/storage.h"
+
+typedef enum {
+    ML_FS_RAMFS,
+    ML_FS_ISO9660,
+    ML_FS_FAT12,
+    ML_FS_FAT16,
+    ML_FS_FAT32,
+    ML_FS_EXT,
+    ML_FS_EXT2,
+    ML_FS_EXT3,
+    ML_FS_EXT4,
+    ML_FS_VFS
+} ml_fs_type;
+
+typedef struct {
+    metalanguage_t (*init)(sdev_ident_t disk, ml_fs_type fs_type);
+
+} metalang_fs_t;
+
+
+#endif
